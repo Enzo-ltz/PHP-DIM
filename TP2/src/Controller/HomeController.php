12a31b10dd8bc31dcf6fddfa5278280d3d256c4d
Enzo <enzo.lentzy.83@gmail.com>
2021-02-08 10:10:33 +0100

    initializing first class and composer

---
+++ TP2/src/Controller/HomeController.php
@@ -0,0 +1,12 @@
+<?php
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+
+class HomeController
+{
+    public function index(Request $request): Response {
+
+        return new Response('it works');
+    }
+}
---
+++ TP2/vendor/composer/installed.php
@@ -0,0 +1,69 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '0b518d626aa30b053ef5adca378fda81cfc865c4',
+    'name' => 'enzo/tp2',
+  ),
+  'versions' => 
+  array (
+    'enzo/tp2' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '0b518d626aa30b053ef5adca378fda81cfc865c4',
+    ),
+    'symfony/deprecation-contracts' => 
+    array (
+      'pretty_version' => 'v2.2.0',
+      'version' => '2.2.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '5fa56b4074d1ae755beb55617ddafe6f5d78f665',
+    ),
+    'symfony/http-foundation' => 
+    array (
+      'pretty_version' => 'v5.2.3',
+      'version' => '5.2.3.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '20c554c0f03f7cde5ce230ed248470cccbc34c36',
+    ),
+    'symfony/polyfill-mbstring' => 
+    array (
+      'pretty_version' => 'v1.22.0',
+      'version' => '1.22.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'f377a3dd1fde44d37b9831d68dc8dea3ffd28e13',
+    ),
+    'symfony/polyfill-php80' => 
+    array (
+      'pretty_version' => 'v1.22.0',
+      'version' => '1.22.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'dc3063ba22c2a1fd2f45ed856374d79114998f91',
+    ),
+    'symfony/var-dumper' => 
+    array (
+      'pretty_version' => 'v5.2.3',
+      'version' => '5.2.3.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '72ca213014a92223a5d18651ce79ef441c12b694',
+    ),
+  ),
+);
